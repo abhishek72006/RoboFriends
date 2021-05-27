fce3283299fcb8adb0623426ae5811b63baf017a
abhishek <abhishekwagle2015@gmail.com>
2021-05-27 22:18:24 +0545

    fist react command

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <Hello greeting={'hello react ninja'}/>,
   document.getElementById('root')
 );
 
