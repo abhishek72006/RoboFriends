7cfa016c1f605c0551227f041d869d28faf54402
abhishek <abhishekwagle2015@gmail.com>
2021-06-03 08:57:39 +0545

    searchbox and heading added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
-import {robots} from './robots';
 
-ReactDOM.render(
-  <CardList robots={robots}/>
-,document.getElementById('root')
+
+ReactDOM.render(<App/>,document.getElementById('root')
 );
